f10b50f488d5e1b07a7d78363686fe4d4daea683
icyice777 <jonathan.liu.777777.7@gmail.com>
2026-06-10 19:32:23 +0800

    Optimize log file size

--- oracle_cloud_instance_creator.sh
+++ oracle_cloud_instance_creator.sh
@@ -32,6 +32,10 @@ profile="$PROFILE" # OCI CLI profile name, default is "DEFAULT"
 
 # ----------------------ENDLESS LOOP TO REQUEST AN ARM INSTANCE---------------------------------------------------------
 
+text="Working hard to get you an instance up and running... Please wait. "
+length=${#text}
+i=0
+
 while true; do
 
     error_output=$(oci compute instance launch --no-retry  \
@@ -52,21 +56,31 @@ while true; do
 
     # Check if the command was successful
     if [[ "$exit_code" -eq 0 ]]; then
+        echo ""
         echo "$(date '+%Y-%m-%d %H:%M:%S'): Instance created successfully! Exiting."
         exit 0
     fi
 
     if echo "$error_output" | grep -qi "Out of host capacity"; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S'): Out of host capacity. Retrying in $requestInterval seconds..."
+        char="${text:$i:1}"
+        echo -n "$char"
+        ((i++))
+        if [ $i -eq $length ]; then
+            i=0
+        fi
+        #echo "$(date '+%Y-%m-%d %H:%M:%S'): Out of host capacity. Retrying in $requestInterval seconds..."
         sleep $requestInterval
     elif echo "$error_output" | grep -qi "TooManyRequests\|429"; then
+        echo ""
         echo "$(date '+%Y-%m-%d %H:%M:%S'): TooManyRequests. Retrying in $backoffTime seconds..."
         sleep $backoffTime
     elif echo "$error_output" | grep -qi "InvalidParameter\|LimitExceeded\|NotAuthorizedOrNotFound"; then
+        echo ""
         echo "$(date '+%Y-%m-%d %H:%M:%S'): InvalidParameter, LimitExceeded or NotAuthorizedOrNotFound error. Check your setup and config again! Exiting."
         echo "Error details: $error_output"
         exit 1
     else
+        echo ""
         echo "$(date '+%Y-%m-%d %H:%M:%S'): An unexpected error occurred. Check the error message below and adjust your setup if necessary. Retrying in $backoffTime seconds..."
         echo "Error details: $error_output"
         sleep $backoffTime
