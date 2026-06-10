2cebcc1c2c4858dbed5efabd109e8c9e177f3a8e
icyice777 <jonathan.liu.777777.7@gmail.com>
2026-06-10 21:26:46 +0800

    Refine script

--- oracle_cloud_instance_creator.sh
+++ oracle_cloud_instance_creator.sh
@@ -32,12 +32,15 @@ profile="$PROFILE" # OCI CLI profile name, default is "DEFAULT"
 
 # ----------------------ENDLESS LOOP TO REQUEST AN ARM INSTANCE---------------------------------------------------------
 
-text="Working hard to get you an instance up and running... Please wait. "
-length=${#text}
+# text="Working hard to get you an instance up and running... Please wait. "
+# length=${#text}
 # length=$(echo "$text" | wc -c)
 # wc -c counts the newline, so we subtract 1 to get exact string length
 # length=$((length - 1))
-i=0
+# i=0
+
+echo "Working hard to get you an instance up and running... Please wait. "
+echo ""
 
 while true; do
 
@@ -59,36 +62,32 @@ while true; do
 
     # Check if the command was successful
     if [[ "$exit_code" -eq 0 ]]; then
-        echo ""
         echo "$(date '+%Y-%m-%d %H:%M:%S'): Instance created successfully! Exiting."
         exit 0
     fi
 
     if echo "$error_output" | grep -qi "Out of host capacity"; then
-        char="${text:$i:1}"
+        # char="${text:$i:1}"
         # 1. Extract exactly one character (POSIX sh safe)
         # We use cut because it is lightweight and built into almost all containers
         # char=$(printf "%s" "$text" | cut -c $((i + 1)))
-        echo "$char"
+        # echo "$char"
         # printf "%s" "$char"
-        ((i++))
+        # ((i++))
         # i=$((i + 1))
-        if [ $i -eq $length ]; then
-            i=0
-        fi
-        #echo "$(date '+%Y-%m-%d %H:%M:%S'): Out of host capacity. Retrying in $requestInterval seconds..."
+        # if [ $i -eq $length ]; then
+        #     i=0
+        # fi
+        echo -ne "$(date '+%Y-%m-%d %H:%M:%S'): Out of host capacity. Retrying in $requestInterval seconds...\r"
         sleep $requestInterval
     elif echo "$error_output" | grep -qi "TooManyRequests\|429"; then
-        echo ""
         echo "$(date '+%Y-%m-%d %H:%M:%S'): TooManyRequests. Retrying in $backoffTime seconds..."
         sleep $backoffTime
     elif echo "$error_output" | grep -qi "InvalidParameter\|LimitExceeded\|NotAuthorizedOrNotFound"; then
-        echo ""
         echo "$(date '+%Y-%m-%d %H:%M:%S'): InvalidParameter, LimitExceeded or NotAuthorizedOrNotFound error. Check your setup and config again! Exiting."
         echo "Error details: $error_output"
         exit 1
     else
-        echo ""
         echo "$(date '+%Y-%m-%d %H:%M:%S'): An unexpected error occurred. Check the error message below and adjust your setup if necessary. Retrying in $backoffTime seconds..."
         echo "Error details: $error_output"
         sleep $backoffTime
